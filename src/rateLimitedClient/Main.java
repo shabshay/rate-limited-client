39323a3f7f93665509a29c9ee9233c2af886639d
shay shabtay <shay.shabshay@gmail.com>
2019-09-28 22:06:41 +0300

    first commit

---
+++ src/rateLimitedClient/Main.java
@@ -0,0 +1,90 @@
+package rateLimitedClient;
+
+import java.io.IOException;
+
+import org.apache.commons.cli.*;
+
+public class Main {
+
+    private static String serverAddress = "http://localhost:8080/?clientId=";
+    private static int numberOfClients;
+    private static Thread[] clientsThreads;
+
+    public static void main(String[] args) {
+
+        try {
+            setArguments(args);
+            clientsThreads = new Thread[numberOfClients];
+            showWelcomeMessage();
+            startAllClients();
+            waitForExit();
+            terminateClients();
+            System.exit(0);
+        } catch (InterruptedException | IOException e) {
+            System.out.printf("Error while trying to simulate clients: %s%n", e.getMessage());
+        }
+    }
+
+    private static void setArguments(String[] args) {
+        Options options = new Options();
+        Option input = new Option("c", "clients", true, "number of clients to simulate");
+        input.setRequired(true);
+        options.addOption(input);
+
+        input = new Option("a", "address", true, "server address (default value: http://localhost:8080/?clientId=)");
+        input.setRequired(false);
+        options.addOption(input);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+            String clientsArg = cmd.getOptionValue("clients");
+            numberOfClients = Integer.parseInt(clientsArg);
+
+            String addressArg = cmd.getOptionValue("address");
+            if (addressArg != null){
+                serverAddress = addressArg;
+            }
+        } catch (Exception e) {
+            if (e instanceof ParseException) {
+                System.out.println(e.getMessage());
+            }
+            formatter.printHelp("rate limited clients simulator", options);
+            System.exit(1);
+        }
+    }
+
+    private static void waitForExit() throws IOException {
+        //noinspection ResultOfMethodCallIgnored
+        System.in.read();
+        System.out.println("Closing the program...");
+    }
+
+    private static void startAllClients() {
+        for (int i = 0; i < numberOfClients; i++) {
+            clientsThreads[i] = new Thread(new clientSimulator(Integer.toString(i + 1), serverAddress));
+            clientsThreads[i].start();
+        }
+    }
+
+    private static void showWelcomeMessage() throws InterruptedException {
+        System.out.printf("Simulating %d clients to %s%n", numberOfClients, serverAddress);
+        System.out.println("Press any key to exit");
+        System.out.print("Starting simulator in 3..");
+        Thread.sleep(1000);
+        System.out.print("2..");
+        Thread.sleep(1000);
+        System.out.println("1..");
+        Thread.sleep(1000);
+    }
+
+    private static void terminateClients() throws InterruptedException {
+        clientSimulator.stopAllClients();
+        for (int i = 0; i < numberOfClients; i++) {
+            clientsThreads[i].join();
+        }
+    }
+}
---
+++ src/rateLimitedClient/clientSimulator.java
@@ -0,0 +1,68 @@
+package rateLimitedClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Random;
+
+public class clientSimulator implements Runnable {
+
+    private static boolean running = true;
+    private static Random random = new Random();
+    private String uri;
+    private String clientId;
+
+    public clientSimulator(String clientId, String uri) {
+        this.uri = uri;
+        this.clientId = clientId;
+    }
+
+    @Override
+    public void run() {
+        int i = 0;
+        while (running) {
+            i++;
+            createNewRequest(i);
+            doWait();
+        }
+    }
+
+    private void doWait() {
+        int seconds = random.nextInt(7);
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            System.out.println("Failed to wait random number of seconds");
+        }
+    }
+
+    private void createNewRequest(int requestNumber) {
+        String status = " - Success";
+        try {
+            sendRequest(uri + clientId);
+        } catch (Exception e) {
+            status = " - Failed";
+        }
+
+        System.out.printf("Client[%s], Request #%d%s%n", clientId, requestNumber, status);
+    }
+
+    public static void stopAllClients(){
+        running = false;
+    }
+
+    public static String sendRequest(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
+}
